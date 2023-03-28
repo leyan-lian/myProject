41919b2b426bda5235ae348c9b0c07723965b415
leyan <2633501068@qq.com>
2023-03-28 21:58:09 +0800

    创建项目

---
+++ myProject/Controllers/hradminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myProject.Controllers
+{
+    public class hradminController : Controller
+    {
+        // GET: hradmin
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
